6cac576fc41ea789a2052538fdac33b2b96c8984
Innocentia <innocentiahan@gmail.com>
2025-07-24 17:01:39 +0800

    Add WebsiteColors class for website color constants

---
+++ scriptofthesoul/lib/core/constants/colors.dart
@@ -0,0 +1,7 @@
+class WebsiteColors {
+  // Colors used in the website
+  static const String primary = "#";
+  static const String secondary = "#";
+  static const String accent = "#";
+  // Font colors
+}
